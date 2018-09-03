fd60471663cd0921a6fe411513e39ba1cf92cd06
davidgalantin1 <35294591+davidgalantin1@users.noreply.github.com>
2018-09-03 10:06:59 +0200

    Add files via upload

---
+++ Test.java
@@ -0,0 +1,14 @@
+package fr.epsi;
+import java.text.Annotation;
+
+import fr.epsi.App;
+
+public class Test<Addition> {
+	@Test
+		public void addition() {
+		Addition addition = new Addition();
+		addition.setA(1);
+		addition.setB(2);
+	}
+	
+}
